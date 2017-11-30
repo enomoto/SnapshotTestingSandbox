ff55869b1dac5f41b71ab3b6d514ee2b451801ee
enomoto <kenta.enomoto@classi.jp>
2017-11-30 19:27:40 +0900

    add spec

--- SnapshotTestingSandboxTests/SnapshotTestingSandboxTests.swift
+++ SnapshotTestingSandboxTests/SnapshotTestingSandboxTests.swift
@@ -6,31 +6,24 @@
 //  Copyright © 2017年 kenta.enomoto. All rights reserved.
 //
 
-import XCTest
+import UIKit
+import Quick
+import Nimble
+import Nimble_Snapshots
+
 @testable import SnapshotTestingSandbox
 
-class SnapshotTestingSandboxTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+
+final class ViewControllerSpec: QuickSpec {
+    override func spec() {
+        var sut: ViewController!
+        describe("ViewController") {
+            let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+            sut = storyboard.instantiateInitialViewController() as! ViewController
+
+            it("view exists") {
+                expect(sut).notTo(beNil())
+            }
         }
     }
-    
 }
