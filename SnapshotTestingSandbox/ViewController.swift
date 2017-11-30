b6d77e568e2520d324f5953b5947e22d5dac4610
enomoto <kenta.enomoto@classi.jp>
2017-11-30 18:38:23 +0900

    Initial Commit

---
+++ SnapshotTestingSandbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapshotTestingSandbox
+//
+//  Created by kenta.enomoto on 2017/11/30.
+//  Copyright © 2017年 kenta.enomoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
